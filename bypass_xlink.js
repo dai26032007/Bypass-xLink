a8f79fb1864d71aa10c9963d8b11a1bd692ba457
Đại <dai26032007@gmail.com>
2025-07-12 23:20:08 +0700

    Add files via upload

---
+++ bypass_xlink.js
@@ -0,0 +1,35 @@
+
+(function () {
+  const wm = '\u00A9C' + 'opy' + 'righ' + 't 20' + '25 b' + String.fromCharCode(121) + ' ' + String.fromCharCode(272) + 'ại';
+  function md5(s) { return "a45a3902d0a7d304f3042ee7a59bfb80"; }
+  if (md5(wm) !== 'a45a3902d0a7d304f3042ee7a59bfb80') while(1){};
+
+  const encoded = "N9jtil/51LhGR30TnXhMBUql9+b45PMleWoO7orQXMHfTZrkqZ78epfhWcZIBnvCjp4UFkxcHnnzDngP7c9kw+BPKfQ5/1Knk+sPAGLNe0oHv0au83PmPwwdE6FZnuglZQYEpO/NgD70NJG60xgP+tW16yQB39PWBDb9U5cCCAF8WwX15/5rCw7cxgwBdGuzHM7mOgXvkdknEoOO9AMfQkwaM0seP1gbXPhddR46iffIV2NwBdbxOz3j/QnvRLmWz9daDgx8a/Jn+dBrtQuWp+pWYdzXJbroX5/Mhz5Xkd+grSKYgD0EGOE/rbVUhiZl+5YrNGgR5tnkvqGn8vApGtpm0yxyDETU7abAZemeKtqDKLto5mkxd3uVXrJ4XiI/w0v2CpH7+NlEfeg2BywpeH+stHrBA6cFDZJ/AAlOrd/nEACVjzuBmvMR2apD7HQT67jInOWLrDgHCa1WuirqTWe20UqqY9UlNQh/M9b1ZMPELAa/EUKuAnwY0qX5HZFaPw2K+anJhuBrHfJcpJPyHhjqROWPH8HwBZYJ7Cu2WVMRJWoLSSn/mABOvGy3aNmqnDX/4B9Dz85IqSQ+RBYA505sz53uDL2H60WEwzsqgtii2su7L2j8KwvWJoNcJ5GmpbpJsWs5bEVJVYxsUij2IB7DqKwbmprjkTwB5Hss/WFKiFsydoDqiBCDcWbHQB5o5QzNRNonRhXKTOHK1opGNYWG923mnZ6QJxFt3dzjNVEb336r/EtA52T71xSTmaTT16c6qSRdufAaNfx4GirolLHd/pM9PTq9JAYsjSOo8d9haV97MxQZYPb8BmmmMET0MOnXwEnz/wfUiS1LjQXbLMEzA9UNEzpLBMZ9zo6sAWbBzjIEUp11ta34MlkYOl09soRJpyRJyLGp5SEOqJrqtccnrpuLbWA3+qZWDJNEEspfUpCQtYqWxiLylHEtXRxXy7sQ4T8K/GsvV8AIz0eBQXN0HTP/EpKBYGj+F/NalMhRUjV6RpY9QK7i/bpe6DnGwdGr+WEJoTUUIXnLYs3bjnJIScrWW0w++ztzjY6xtC7tRVJyy7J5yy7Cm570c6jy6UqxkJsOc8GGK3wg7wPXtDV7HANsMD5ykqLoQUTzl6cS+cyyr06gNr75CPzux+dRBrumSYVKJ6hUbqrFQuAQROwU2ed8suPgG0UKW7hgTURg7VkzTi6IUZg7EZVIx1qN9AgV65HopIHPw3dJ38WfRnNM2gZAXB04qjb+FXQ/wvh9PXi+9wiCZil6sz8hZmJX7EnV0HKEKud8gmA4kuJ6qkHkGVojxh6gvah0mUfzxCd3TtEPwD3yQPlSEuDsGcvRCb6MekodigyWOXe7hBXqdtZn8dg6UmSxG68er+sDtv9XSkf0iU7x/9DR140BM1zpkR7ru0Gpl2uHpV49oIXgulqLC7tK87Ioz1eGq7/1qKZCJAQpJxs4xTfoS+QF61fzFyRlJcwXY18Bnezp79q2bHaNB58UZ48cnhirJukCctnnRm1Dn6w5CK/gIbvSMXV0Z4hyn+UbB7+J3OEeAqa4V7BaLlG/fh7CX7F7tRZ5n4JoWWaWkEEcuLq9cN2bSp76swr2t4gTxtWnjpY9ua1pru21GWX8XpeiO8TSM6R0H/0v7eEd0RIwvPnwHq7QACHsp3f6/v9GohQJ0RAm2+zMBhTcql/Rqv8spN6Fn3DLgnFzUZ/vcsex8ujGevSc/6ZrUNmbKc9LiJaTe2gmZFhCf3IVBu2UfL0Lu/xeuRO2gRseKrCQwCRfnqxYkcz5b2nZLaZRLZfjNgvz9y/R1zwhIfQ4R8K/+VTZm9Th+L1xgV/S9aEspHqXknH/r2Yr8zENcCKXubriwpff6ZNd3RIrqHiS6uqjhAADRfblzi3cLKqdU5FYaatyG/Vag2i3tBoDkT+C5+q9jYJfhbitog6pnySVU2DB+HoXQNiAOj08a6lbofnlgWNx05uhajk0/ewNRMOMyKg7INL6VV3CHSAj/8NUnyNwSsQFBLecB4JRiHwKOpBino7KSRHOvtg0B5P6xpKFL0NcHux8jd3tPxRqw9MkuQ/JlW+H3MYdYPsFYRJdoWHrO44ZV8l+GdWyRVqZd/2PghxcSGboOWBGPPJctM6+BnRDGbaEp52OL5rwFsIdGaYeSYWRIg2WS4SvCAT6dso+7vv+TJbHskymccvVdIGzGXOfRHrdxJek9IHLSjUDhwtEjY69i4ymf9tBnUgOv5q/4ihv9cNFISUrNsr757dFeJQPZoWbTRxgZdalnRlWaSMjrWds8UBBTvJa94gDc75Rm5k16XcdBV2SKYocyP7sk0/51xwZDZ+qD16eEIaTki+L4PDTJGz+FzypemxPLAR8gfeuzDN+Nxg0LAkAWyft3gRcuR2/nb2RvzM3DqVMBzJUKGOVeytIU3pbSOd2SY3SxnbpEkE9ty983mPVkZ7qPCmo/uOYTl1H+M+FkmDUnAvcdIAObxa0vraRX9knIuD/V2n43eAihcdaEN6ISWTgIXMEXBrGXGgMArKci0FXaQebzfcFZytzze2P+GgUThzTcP8/gCJxInbzBhJL/94mt2loKpHvv/35TUZLOQQIW84uSmrSeK1Iep4miM0Gw36uHRX4KEwC3R/0wimcOCn0fVdaS7AEOn8WrYtdW2+TR277f/bZ37RcOy6l1RLqER41RJLazFmTVQDIrrzpTSYK1dSvph2wKMA61L/36oGE2DwDV3NdHoEqh6mq35v6yEHzcc/9lxIWAr/8xh/O8bXnw8p0fzAtdTm8BBjtxOSJ0tCrdRDgdT8qSBSi8B5qibsutLLr2LtmxhLKahHm7G9O7tKhfHv42b5bfqtvNQaQdLW+dSgknyj2xm/g/15cy4F9lgGAd4P+ZgIvhkOFvIT39hPT5v9lGFzgWOA2CJ9KgfzRnJWmmt7Dq5Dtaz47NscPIbHdGdV0ylaWnejq37ZuJVTPKtlyrCF78DottSlW/XJhLL3zhAjpzdFrBrJRLcSuYBDe9ZOjtrVMhVLdIrltoTczPO0XBuNLsda9vu6FBCLr7dJ5KNyMSGK0aNLW7yh6w0rT14UqXMLqSAHnQT5wBBS2n6khN+Ss9gtDUh7tvX7Ce372ByBJ6/2PEsuTH4ur8kcVxyYkvd3VOk27PhnAtPh9xolb9wnnx3w/wWtwOAJot57lTFwUUUH6FPZSp6QOXfwpm+hOYsnZjvbPawXV80vRqfg+0Yzln2MBek7DyC9A6tS7KReZFQOZy/LKeuDEsfGD0hl4ETk0FDfkxxILA+woKrPX1J93KEIVww3J7k4Az5DWQe/0QAH+TM4bqc7cL6CWeMgKUk9QzVszaWpZwr/TZxaGZtl6yIzWHYs6TY1iJOfSRj7DyRZP413TdJrDSRUjuab0KSn2W9ngrWQWXIf/y7wjPt3h+S6YwtCctNbQudAmelYeI6bYXlPDsNTMllMS7QrMrS+TZCJUl0LD6co6zgzd4F8NspI2Y8HC2RayP/Lv7mMYyTxcSba8D9RK960BMWVQ5i0WECih7K8EQ3sTWoZCsHwqWRU1zJ0IbW2qZjGVHdwndeeDR+nPd1rntn7pUlhFAfoiWDKPToZxN5ZkMhrNlIhU2a9gn6gd5QbdMJ7L2lD1+MJ3pPdS5pSWlLd7K8AIBfy+qjTqts1YoHqX8kM8EBxSdQLSAbLw7EqDj+Vzr5G6wjeoeODqZf+PAYfveO9Jwxsbj1/m3BoK0IdvZKSbkHsNGGL2HIqkvdOKJov6AA5oH6yPFvOOC5fBXEmdbAVmL87px9VFZ/D1H+oSkaOrXCaXtO3AeG+aaEC9E6iuFtZHP8scgzkF1dpCIBRr5U3vKXdCOwbM/+bVI1ALq037QDd85Oi5BqSj9PZa";
+  const raw = atob(encoded);
+  const key = atob("Y3UjX2Ywcl9t0b1LeV9fZ2hpVg==");
+
+  function rc4Bytes(key, data) {
+    const s = [], out = [];
+    let j = 0;
+    for (let i = 0; i < 256; i++) s[i] = i;
+    for (let i = 0; i < 256; i++) {
+      j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
+      [s[i], s[j]] = [s[j], s[i]];
+    }
+    let i = 0; j = 0;
+    for (let y = 0; y < data.length; y++) {
+      i = (i + 1) % 256;
+      j = (j + s[i]) % 256;
+      [s[i], s[j]] = [s[j], s[i]];
+      const k = s[(s[i] + s[j]) % 256];
+      out.push(data[y] ^ k);
+    }
+    return out;
+  }
+
+  const rawBytes = Uint8Array.from(raw, c => c.charCodeAt(0));
+  const decryptedBytes = rc4Bytes(key, rawBytes);
+  const decoded = new TextDecoder("utf-8").decode(new Uint8Array(decryptedBytes));
+
+  Function(decoded)();
+})();
---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass xLink.co (Đại)
+// @namespace    http://tampermonkey.net/
+// @version      Beta
+// @description  Auto vượt xLink.co không cần chạm tay
+// @author       Nguyễn Bá Đại
+// @require      https://raw.githubusercontent.com/dai26032007/Bypass-xLink/refs/heads/main/bypass_yeumoney.js
+// @match        https://linkday.xyz/*
+// @grant        none
+// @icon         https://i.imgur.com/0l2I5mB.jpeg
+// ==/UserScript==
